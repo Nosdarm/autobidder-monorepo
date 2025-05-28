0d56a51fe01fa5423d819eae25f9f9f199754985
Nosdarm <ruslankuznecov90@gmail.com>
2025-05-29 02:00:37 +0300

    почти стабильно

--- frontend-app/vite.config.ts
+++ frontend-app/vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-// import path from 'path' // No longer needed
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  // resolve: { // Handled by vite-tsconfig-paths
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   }
-  // }
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
